5289521d2d5e10324a4d73ec7349851077db42fe
osidekyle <hawkinskyle67@gmail.com>
2020-05-27 23:03:01 -0700

    transitions for delete and check buttons

--- app.js
+++ app.js
@@ -52,7 +52,12 @@ function deleteCheck(e){
     //Delete todo
     if(item.classList[0]==='trash-btn'){
         const todo=item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend',function(){
+            todo.remove();
+        });
+        
     }
 
 
